3975cfc5ca2620fea0f246873d0364fd4d727f76
jdhtoro <juandhtoro@gmail.com>
2024-03-21 21:28:35 -0500

    first commit

---
+++ src/constants/api.js
@@ -0,0 +1,8 @@
+
+const JPG = "image/jpeg";
+const PNG = "image/png";
+
+export {
+    JPG,
+    PNG,
+};
---
+++ src/hooks/useProducts.js
@@ -0,0 +1,65 @@
+import { useContext } from "react";
+import useLocalStorage from "./useLocalStorage";
+import ShoppingCartContext from "../contexts/ShoppingCartContext.jsx";
+import axios from "axios";
+
+const useProducts = () => {
+    const { items, setItem } = useLocalStorage({ products: [] });
+    const { removeCartProduct } = useContext(ShoppingCartContext);
+
+    const searchProducts = async (params) => {
+        try {
+            const queryParams = new URLSearchParams(params);
+            const response = await axios.get(`"https://mitienda-juan.onrender.com/api/products?${queryParams.toString()}`);
+            return response.data;
+        } catch (error) {
+            console.error("Error searching products:", error);
+            throw error;
+        }
+    };
+
+    const createProduct = async (values) => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/products", values);
+            setItem("products", [ ...items.products, response.data.data ]);
+        } catch (error) {
+            console.error("Error creating product:", error);
+            throw error;
+        }
+    };
+
+    const updateProduct = async (values) => {
+        try {
+            const response = await axios.put(`http://localhost:3000/api/products/${values.id}`, values);
+            const updatedProducts = items.products.map((product) =>
+                product.id === values.id ? response.data.data : product,
+            );
+            setItem("products", updatedProducts);
+        } catch (error) {
+            console.error("Error updating product:", error);
+            throw error;
+        }
+    };
+
+    const removeProduct = async (id) => {
+        try {
+            await axios.delete(`http://127.0.0.1:3000/api/products/${id}`);
+            const productsWithoutThisProduct = items.products.filter((item) => item.id !== id);
+            setItem("products", productsWithoutThisProduct);
+            removeCartProduct(id);
+        } catch (error) {
+            console.error("Error removing product:", error);
+            throw error;
+        }
+    };
+
+    return {
+        products: items.products,
+        searchProducts,
+        createProduct,
+        updateProduct,
+        removeProduct,
+    };
+};
+
+export default useProducts;
